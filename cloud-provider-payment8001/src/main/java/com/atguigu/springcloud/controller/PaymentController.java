cfa2ca7238784a798f89608ef8a5d3a7f7aa5bfb
Leonzhurui <leonzhurui@qq.com>
2022-04-12 16:50:42 +0800

    Test

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -71,4 +71,9 @@ public class PaymentController {
         return serverPort;
     }
 
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin(){
+        return "hi, i am paymentzipkin";
+    }
+
 }
