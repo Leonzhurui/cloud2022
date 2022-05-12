556cf61cf367c4c48d16ae17eb3411c041715e2c
Leonzhurui <leonzhurui@qq.com>
2022-05-12 17:55:04 +0800

    T

--- cloud-alibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.alibaba.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class FlowLimitController {
 
@@ -14,6 +16,16 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "------------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "------------testD";
     }
 
 }
