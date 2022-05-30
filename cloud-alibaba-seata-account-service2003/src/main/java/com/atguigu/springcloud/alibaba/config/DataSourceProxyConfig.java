2e27bb0723e8317c5cb1e51d420cb2b2d6bb9232
Leonzhurui <leonzhurui@qq.com>
2022-05-30 09:16:18 +0800

    T

--- cloud-alibaba-seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/config/DataSourceProxyConfig.java
+++ cloud-alibaba-seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/config/DataSourceProxyConfig.java
@@ -4,14 +4,19 @@ import com.alibaba.druid.pool.DruidDataSource;
 import io.seata.rm.datasource.DataSourceProxy;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceProxyConfig {
+
+    @Value("${mybatis.mapper-locations}")
+    private String mapperPath;
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
@@ -27,6 +32,7 @@ public class DataSourceProxyConfig {
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSourceProxy dataSourceProxy) throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperPath));//"classpath:mapper/*.xml"
         sqlSessionFactoryBean.setDataSource(dataSourceProxy);
         return sqlSessionFactoryBean.getObject();
     }
