4432ed9b3d662664bc02fc08ebc5992b46cd3976
Leonzhurui <leonzhurui@qq.com>
2022-05-30 10:06:51 +0800

    T

--- cloud-alibaba-seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/controller/StorageController.java
+++ cloud-alibaba-seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/controller/StorageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
-public class StoreageController {
+public class StorageController {
 
     @Resource
     private StorageService storageService;
