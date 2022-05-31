1351a03cc85ea48c418bcc90c295a2a54d279462
Leonzhurui <leonzhurui@qq.com>
2022-05-31 10:08:08 +0800

    T

--- cloud-alibaba-seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ cloud-alibaba-seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class AccountServiceImpl implements AccountService {
@@ -16,6 +17,11 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void decrease(Long userId, BigDecimal money) {
         System.out.println("-------->开始扣减账户余额");
+        try {
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         accountDao.decrease(userId, money);
         System.out.println("-------->开始扣减账户余额end");
     }
--- cloud-alibaba-seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/service/impl/OrderServiceImpl.java
+++ cloud-alibaba-seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/service/impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import com.atguigu.springcloud.alibaba.dao.OrderDao;
 import com.atguigu.springcloud.alibaba.domain.Order;
 import com.atguigu.springcloud.alibaba.service.OrderService;
 import com.atguigu.springcloud.alibaba.service.StorageService;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -29,6 +30,7 @@ public class OrderServiceImpl implements OrderService {
      * @param order
      */
     @Override
+    @GlobalTransactional(name = "order-create", rollbackFor = Exception.class)
     public void create(Order order) {
         // 1.新建订单
         System.out.println("------>开始新建订单");
